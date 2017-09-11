e0c59a249a42bf508d9b570169b6ec2872cca41f
Manoj Kumar Patra <patra.manoj0@gmail.com>
2017-09-11 18:25:22 +0530

    Update session.py

--- app/models/session.py
+++ app/models/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Base
+from app.models.models import Base
 
 engine = create_engine('postgresql://dbuser:catalog@localhost/catalog')
 
